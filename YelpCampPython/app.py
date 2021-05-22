90aba6bb511a8ac204b3628ba9a841d7f8dd2179
mkmenta <mkmenta95@gmail.com>
2021-05-22 16:39:50 +0200

    Move the HTTP middleware to utils

--- YelpCampPython/app.py
+++ YelpCampPython/app.py
@@ -1,41 +1,12 @@
-import re
-
 from flask import Flask, render_template
 from mongoengine import connect
 
 from routes.campgrounds import blueprint as campgrounds_blueprint
+from utils import HTTPMethodOverrideMiddleware
 
 app = Flask(__name__)
 
 connect("yelpCamp")
-
-
-class HTTPMethodOverrideMiddleware(object):
-    allowed_methods = frozenset([
-        'GET',
-        'HEAD',
-        'POST',
-        'DELETE',
-        'PUT',
-        'PATCH',
-        'OPTIONS'
-    ])
-    bodyless_methods = frozenset(['GET', 'HEAD', 'OPTIONS', 'DELETE'])
-
-    def __init__(self, app, field='_method'):
-        self.app = app
-        self._regex = re.compile('.*' + field + '=([a-zA-Z]+)(&.*|$)')
-
-    def __call__(self, environ, start_response):
-        method = self._regex.match(environ.get('QUERY_STRING', ''))
-        if method is not None:
-            method = method.group(1).upper()
-            if method in self.allowed_methods:
-                environ['REQUEST_METHOD'] = method
-            if method in self.bodyless_methods:
-                environ['CONTENT_LENGTH'] = '0'
-        return self.app(environ, start_response)
-
 
 app.wsgi_app = HTTPMethodOverrideMiddleware(app.wsgi_app)
 
---
+++ YelpCampPython/utils.py
@@ -0,0 +1,28 @@
+import re
+
+
+class HTTPMethodOverrideMiddleware(object):
+    allowed_methods = frozenset([
+        'GET',
+        'HEAD',
+        'POST',
+        'DELETE',
+        'PUT',
+        'PATCH',
+        'OPTIONS'
+    ])
+    bodyless_methods = frozenset(['GET', 'HEAD', 'OPTIONS', 'DELETE'])
+
+    def __init__(self, app, field='_method'):
+        self.app = app
+        self._regex = re.compile('.*' + field + '=([a-zA-Z]+)(&.*|$)')
+
+    def __call__(self, environ, start_response):
+        method = self._regex.match(environ.get('QUERY_STRING', ''))
+        if method is not None:
+            method = method.group(1).upper()
+            if method in self.allowed_methods:
+                environ['REQUEST_METHOD'] = method
+            if method in self.bodyless_methods:
+                environ['CONTENT_LENGTH'] = '0'
+        return self.app(environ, start_response)
