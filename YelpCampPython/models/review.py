203b5263acbe3104a65681b0b92305cbebe6f917
mkmenta <mkmenta95@gmail.com>
2021-08-04 00:04:01 +0200

    Add reviews with author and authorization

--- YelpCampPython/models/review.py
+++ YelpCampPython/models/review.py
@@ -1,7 +1,10 @@
 from mongoengine import Document, fields
 
+from models.user import User
+
 
 class Review(Document):
+    author = fields.ReferenceField(User)
     body = fields.StringField(required=True)
     rating = fields.FloatField(required=True, min_value=0.)
 
